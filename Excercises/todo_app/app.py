20b379e35d517dda068fe3523a0786d1da8449a7
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-02 05:40:04 -0700

    Updated code to connect to database using SQLAlchemy.

--- Excercises/todo_app/app.py
+++ Excercises/todo_app/app.py
@@ -4,10 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 # Create an application with same name as name of the module
 app = Flask(__name__)
 
+# Config the URI of the postgres database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://student:student@localhost:5432/studentdb'
+
+# Create a database object which links SQLAlchemy to our current app
+db = SQLAlchemy(app)
+
 @app.route("/")
 def index():
     # Flask will look for this file in templates folder
-    render_template("index.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
