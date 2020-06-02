d1c149b54b02a02f853116f189d9f5baef7486b4
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-02 05:47:12 -0700

    Add the code to model the table in the database

--- Excercises/todo_app/app.py
+++ Excercises/todo_app/app.py
@@ -10,6 +10,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://student:student@localhost:54
 # Create a database object which links SQLAlchemy to our current app
 db = SQLAlchemy(app)
 
+# Model the todo class
+class Todo(db.Model):
+    __tablename__ = "todos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    # Format the objects while printing
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+# Create model of todo class in database
+db.create_all()
+
+
+# Route the home page of the website to this decorator
 @app.route("/")
 def index():
     # Flask will look for this file in templates folder
