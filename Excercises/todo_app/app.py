b28f123acce0d09917005c5ddaa4b0c3551259f2
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-02 05:24:31 -0700

    Updated code to render the index html page when client visits the home page of the application.

--- Excercises/todo_app/app.py
+++ Excercises/todo_app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# Create an application with same name as name of the module
 app = Flask(__name__)
 
-
+@app.route("/")
+def index():
+    # Flask will look for this file in templates folder
+    render_template("index.html")
 
 
 if __name__ == '__main__':
