2bb2bd38a42b3feaf1572c7130b4ef33f047bb3d
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-08 23:28:42 -0700

    Updated code to Delete Venue by id

--- Project01-Fyyur/app.py
+++ Project01-Fyyur/app.py
@@ -260,7 +260,23 @@ def create_venue_submission():
 def delete_venue(venue_id):
     # TODO: Complete this endpoint for taking a venue_id, and using
     # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-    
+    error = False
+    try:
+        Venue.query.filter_by(id=venue_id).delete()
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+    finally:
+        db.session.close()
+        if error:
+            flash('An error occurred. Venue with ' +
+                  venue_id + ' could not be deleted.')
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+
+    # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
+    # clicking that button delete it from the db then redirect the user to the homepage
+    return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
