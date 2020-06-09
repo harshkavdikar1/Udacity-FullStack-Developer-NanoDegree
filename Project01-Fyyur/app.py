3efe2c4c0bb573fe5216d4025173e31a20b2d5e1
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-09 01:07:15 -0700

    Updated code to search artists based on their name

--- Project01-Fyyur/app.py
+++ Project01-Fyyur/app.py
@@ -298,14 +298,26 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+
+    word = "%" + request.form.get("search_term", "") + "%"
+
+    artists = Artist.query.filter(Artist.name.ilike(word))
+
+    data = []
+
+    for artist in artists:
+        print(artists)
+        data.append({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": len([show for show in artist.show if show.start_time>datetime.now()])
+        })
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(data),
+        "data": data
     }
+
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
