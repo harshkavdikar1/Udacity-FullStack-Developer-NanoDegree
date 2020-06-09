fdda2fa5b1359a30e92e2e89b2bdf4dfd92e2e9c
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-09 00:24:22 -0700

    Updated code to view all the artists

--- Project01-Fyyur/app.py
+++ Project01-Fyyur/app.py
@@ -284,16 +284,15 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    all_artists = Artist.query.with_entities(Artist.id, Artist.name).all()
+
+    data = []
+    for artist in all_artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name
+        })
+
     return render_template('pages/artists.html', artists=data)
 
 
