fc39e7f82fad5da271208e48bb40b37b511eaf8a
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-18 04:21:10 -0700

    Created model for movies

--- Capstone-Project/models.py
+++ Capstone-Project/models.py
@@ -3,7 +3,8 @@ from sqlalchemy import Column, String, Integer
 
 
 database_name = "capstone"
-database_path = "postgres://{}/{}".format('student:student@localhost:5432', database_name)
+database_path = "postgres://{}/{}".format(
+    'student:student@localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -20,3 +21,41 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+
+
+class movies(db.Model):
+    __table_name__ = "movies"
+
+    id = Column(db.Integer, primary_key=True)
+    title = Column(db.String(100), nullable=False)
+    release_date = Column(db.DateTime, nullable=True)
+    desc = Column(db.String(1000), nullable=True)
+    rating = Column(db.Integer, nullable=False)
+    actors = db.relationship("actor", secondary=Performance, lazy='subquery',
+                             backref=db.backref('performance', lazy=True))
+
+    def __init__(title, release_date=None, desc=None):
+        self.title = title
+        self.release_date = release_date
+        self.desc = desc
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.commit()
+
+    def format(self):
+
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "description": self.desc,
+            "rating": self.rating
+        }
