abf2ec8b7074279ce3a2e50fa4cea7f16f7c0e5c
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-18 05:43:42 -0700

    Updated test cases for home page.

--- Capstone-Project/test_app.py
+++ Capstone-Project/test_app.py
@@ -1,1 +1,43 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_db, Movie, Actor, Performance
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "capstone_test"
+        self.database_path = "postgres://{}/{}".format(
+            'student:student@localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_home_page(self):
+        res = self.client().get('/')
+
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(
+            data['message'], "Welcome please refer to API documentation for the endpoints")
+
+if __name__ == '__main__':
+    unittest.main()
