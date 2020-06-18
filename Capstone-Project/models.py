383287b9fbefe8b434763492c1d7dcc27b18c1eb
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-06-18 04:00:42 -0700

    Setup db configurations in models.py

--- Capstone-Project/models.py
+++ Capstone-Project/models.py
@@ -1,1 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer
 
+
+database_name = "capstone"
+database_path = "postgres://{}/{}".format('student:student@localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    """
+    Setup db configurations
+    Initialize database to app
+    Create the tables according to models if they don't exist already.
+    """
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
